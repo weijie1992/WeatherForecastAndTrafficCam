5bcd716cee4c73748b4afb649967d5e7ce47a1cf
Wei Jie Phua <weijie_luvbb@hotmail.com>
2024-01-26 14:31:59 +0800

    include basic dockerization for both frontend and backend

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -5,7 +5,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: 'http://localhost:8080',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     });
     await app.listen(3005);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(3005);
